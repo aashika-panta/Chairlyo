7f6701c689e3cf2b6840a1d656f32ddf738053a2
Aashka Panta <aashikapanta1973@gmail.com>
2026-09-01 18:11:05 +0545

    work on branch CRUD Functionality

--- cypress/support/selector.js
+++ cypress/support/selector.js
@@ -10,18 +10,45 @@ export const forgetpassword = {
 };
 
 export const dashboardselector = {
-  profileicon:"#radix-_r_45_ > aside > div.py-1.border.rounded-xl.border-neutral-200.overflow-hidden.hover\:bg-neutral-100.cursor-pointer.transition-\[background\].shrink-0 > div",
+  profileicon:
+    "#radix-_r_45_ > aside > div.py-1.border.rounded-xl.border-neutral-200.overflow-hidden.hover\:bg-neutral-100.cursor-pointer.transition-\[background\].shrink-0 > div",
   logout: "//button[normalize-space()='Logout']",
   logoutclick: '//*[@id="radix-_r_53_"]/div[2]/button[2]',
-  sidebarmenu:"/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/div[1]/div[2]/button[2]/span[1]/*[name()='svg'][1]",
-  branchmenu: "/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/div[2]/div[1]/nav[1]/div[1]/a[1]/span[1]/*[name()='svg'][1] ",
-  addBranch: "//div[contains(@class,'lg:flex gap-2')]//button[normalize-space()='Add Branch']",
+  sidebarmenu:
+    "/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/div[1]/div[2]/button[2]/span[1]/*[name()='svg'][1]",
+  branchmenu:
+    "/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/div[2]/div[1]/nav[1]/div[1]/a[1]/span[1]/*[name()='svg'][1] ",
+  addBranch:
+    "//div[contains(@class,'lg:flex gap-2')]//button[normalize-space()='Add Branch']",
 };
 
-export const search={
-  searchclick:"//input[@placeholder='Search...']",
-  
-}
-export const branchselector={
+export const search = {
+  searchclick: "//input[@placeholder='Search...']",
+};
+export const branchselector = {
+  addbranch: '//*[@id="root"]/div/div[2]/main/div/div[1]/div[1]/div[2]/button',
+  Branchname: "#name",
+  Slug: "#slug",
 
-}
+  BranchPhone: '[name="phone"]',
+  BranchEmail: "#email",
+  Status:
+    "/html/body/div[1]/div/div[2]/main/div/div[2]/div/form/div/div/div/div[5]/button",
+  Address: "#address",
+  TimeZone: "//span[@class='flex-1 truncate text-neutral-700']",
+
+  Firstname: "#admin_first_name",
+  Lastname: "#admin_last_name",
+  AdminEmail: "#admin_email ",
+  Password: '[name="admin_password"]',
+  AdminPhone: "[name='phone'] ",
+
+  createbranch: "//button[normalize-space()='Save Changes']",
+
+  editbranch:
+    "//tbody/tr[2]//*[name()='svg']//*[name()='path' and contains(@d,'M12 3H5a2 ')]",
+  updatebranch: "//button[normalize-space()='Save Changes']",
+  deletebranch: "//*[name()='path' and contains(@d,'M10 11v6')]",
+  confirmdelete: "//input[@placeholder='Type Delete Branch here']",
+  delete: "/html[1]/body[1]/div[3]/div[4]/button[2]",
+};
